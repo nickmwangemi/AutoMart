eebcc46b339c23605034e8484d7f83fef7c351aa
nickmwangemi <nickmwangemi@gmail.com>
2020-08-20 22:57:22 +0300

    Initial commit

---
+++ src/main/java/com/mwangemi/automart/AutomartApplication.java
@@ -0,0 +1,13 @@
+package com.mwangemi.automart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mwangemi/automart/AutomartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mwangemi.automart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
